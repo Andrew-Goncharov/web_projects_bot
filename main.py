8648c4c7185efb06e6b3db062e6e00098ec23b1a
Andrew <andrew208132@gmail.com>
2023-03-09 18:33:12 +0300

    add InlineKeyboardButtons to /start handler

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import logging
-# import asyncio
 
-from telegram import Update, InlineQueryResultArticle, InputTextMessageContent
-from telegram.ext import filters, MessageHandler, ApplicationBuilder, ContextTypes, CommandHandler, InlineQueryHandler
+from telegram import Update, InlineQueryResultArticle, InputTextMessageContent, InlineKeyboardButton, \
+    InlineKeyboardMarkup
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, ContextTypes, CommandHandler, InlineQueryHandler, \
+    CallbackQueryHandler
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
@@ -10,8 +11,26 @@ logging.basicConfig(
 )
 
 
+async def button(update: Update, _):
+    query = update.callback_query
+    variant = query.data
+    await query.answer()
+    await query.edit_message_text(text=f"Выбранный вариант: {variant}")
+
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm bot, please talk to me!")
+
+    keyboard = [
+        [InlineKeyboardButton("переводы", callback_data="переводы", url="https://student.itmo.ru/ru/transfer/")],
+        [InlineKeyboardButton("БСК", callback_data="БСК", url="https://student.itmo.ru/ru/bsk/")],
+        [InlineKeyboardButton("стипендии", callback_data="стипендии", url="https://student.itmo.ru/ru/scholarship/")],
+        [InlineKeyboardButton("студенческий офис", callback_data="студенческий офис",
+                              url="https://student.itmo.ru/ru/about_studoffice/")]
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text("Сделайте выбор: ", reply_markup=reply_markup)
 
 
 async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -56,6 +75,7 @@ if __name__ == "__main__":
     application = ApplicationBuilder().token("6122726933:AAHVD9YdOReYX2-O2BvFe0k5YWiiQCSzoLA").build()
 
     start_handler = CommandHandler("start", start)
+    callback_handler = CallbackQueryHandler(button)
     caps_handler = CommandHandler("caps", caps)
     help_handler = CommandHandler("help", help)
     echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
@@ -63,6 +83,7 @@ if __name__ == "__main__":
     unknown_handler = MessageHandler(filters.COMMAND, unknown)
 
     application.add_handler(start_handler)
+    application.add_handler(callback_handler)
     application.add_handler(caps_handler)
     application.add_handler(help_handler)
     application.add_handler(echo_handler)
